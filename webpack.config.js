3aa49426921e49f5b721384131beff4475a8f8a8
Cian O'Gralaigh <Cian.OGralaigh@version1.com>
2019-07-29 17:25:27 +0100

    start webpack config

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+module.exports = {
+  devtool: "cheap-module-eval-source-map",
+  entry: "./src/index.html",
+  output: {
+    path: "",
+    filename: "bundle.js",
+    publicPath: path.resolve(__dirname, "dist")
+  },
+  resolve: { extensions: [".js", ".jsx"] }, module: {rules: [ {test: /\.js$/, loader: , exclude: /node_modules/}]}
+};
